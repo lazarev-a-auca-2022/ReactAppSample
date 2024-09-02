a5cc0a0992f6ce46abf523ceb1de519bd491510e
lazarev_a <lazarev_a@auca.kg>
2024-09-02 16:20:40 +0600

    Buttons

--- reactsample/src/App.js
+++ reactsample/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React initialized successfully</h1>
-        <nav>
-          <ul className="navbar">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/shop">Shop</Link></li>
-            <li><Link to="/product">Product</Link></li>
-            <li><Link to="/contact">Contact Us</Link></li>
-          </ul>
-        </nav>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1>React initialized successfully</h1>
+          
+          <Link to="/about">
+            <Button variant="light">Light</Button>{' '}
+          </Link>
+          
+        </header>
+      </div>
+    </>
+    
   );
 }
 
 export default App;
+
